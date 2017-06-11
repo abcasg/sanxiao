b661338f4299e3f85d8dea5864e51cab7cf7b78e
abcasg <1307520486@qq.com>
2017-06-11 22:45:34 +0800

    de

--- src/app.js
+++ src/app.js
@@ -214,7 +214,7 @@ var HelloWorldLayer = cc.Layer.extend({
         if (cubeArry.length < 3) {
             return;
         }
-        EliminateHelper.debugLog();
+        // EliminateHelper.debugLog();
         for (var i = 0; i < cubeArry.length; i++) {
             var objP = cubeArry[i];
             var cube = this.getCubeSpByP(objP);
@@ -228,7 +228,6 @@ var HelloWorldLayer = cc.Layer.extend({
             this.createDownCube();
             return;
         }
-
         //console.log("cubeArry.length " + cubeArry.length + " mdArry.length " + mdArry.length + " p.x " + p.x + " p.y " + p.y);
         for (var i = 0; i < mdArry.length; i++) {
             var obj = mdArry[i];
